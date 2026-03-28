0b137e472863df4723c42143cbbc2d3ab1463c65
Jason Reed <jcreed@gmail.com>
2026-03-28 11:07:30 -0400

    another chrome vibration test

--- public/sw.js
+++ public/sw.js
@@ -2,7 +2,7 @@ self.addEventListener('push', e => {
   const data = e.data?.json() ?? { title: 'Event', body: 'Something happened' };
   e.waitUntil(
     self.registration.showNotification(data.title, {
-      body: data.body,
+      body: data.body + ' (sw-v2)',
       icon: 'icon.png',
       vibrate: data.vibrate || [200, 100, 200],
     })
